8f5c60967a03e00b424170780365754a954e3f12
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-04-05 11:46:24 +0100

    create getter for index attribute

--- graph_game/node.py
+++ graph_game/node.py
@@ -5,25 +5,14 @@ from typing import List, Tuple
 class Node:
     """Creation of a Node class to represent nodes in the graph data structure."""
 
-    def __init__(self, val: int) -> None:
+    def __init__(self, idx: int) -> None:
         """Initializes a Node object with the parameter value.
 
         Args:
-            val: This are the values associated with the node.
+            idx: This are the values associated with the node.
         """
-        self.val = val
+        self.idx = idx
         self.neighbours = {}  # A dictionary to keep the neighbours and their weight
-
-    def __eq__(self, other: type[Node]) -> bool:
-        """Check whether the two nodes have the same values.
-
-        Args:
-            other: Another node in the graph to compare with.
-
-        Returns:
-            True if the value of the other node has the same value as the value of the this node, False otherwise.
-        """
-        return self.val == other.val
 
     def __lt__(self, other: type[Node]) -> bool:
         """Compares two nodes based on their values.
@@ -34,7 +23,7 @@ class Node:
         Returns:
             True if the value of the other node is greater than the value of the this node, False otherwise.
         """
-        return self.val < other.val
+        return self.idx < other.idx
 
     def add_neighbour(self, neighbour: type[Node], weight: int | float) -> None: 
         """Adds a neighbour to the nodes in the graph with an associated weight.
@@ -53,10 +42,18 @@ class Node:
         
         self.neighbours[neighbour] = weight
 
+    def get_index(self) -> int:
+        """A getter method for the idx attribute.
+
+        Returns:
+            An integer index of the node.
+        """
+        return self.idx
+    
     def get_neighbours(self) -> List[Tuple[int, int]]:
         """A getter method for the neighbours attribute.
 
         Returns:
-            A List of tuples (val, wei) consist of the value (val) and the weight (wei) of each node.
+            A List of tuples (idx, wei) consist of the index (idx) and the weight (wei) of each node.
         """
         return self.neighbours.items()
---
+++ graph_game/score_generation.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+
+class ScoreGeneration:
+    def __init__(self) -> None:
+        pass
