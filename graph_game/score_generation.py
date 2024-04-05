1430d2fb008a140edc827fc7c2c3b581c74cba72
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-04-05 14:38:26 +0100

    create a template for RandomScoreGeneration class

--- graph_game/score_generation.py
+++ graph_game/score_generation.py
@@ -1,7 +1,24 @@
-import numpy as np
+from typing import List
+from scipy.stats import norm 
 
 
+class RandomScoreGenerator:
+    def __init__(self, base_score: int = 100) -> None:
+        self._mean = None
+        self._sd = None
+        self.base_score = base_score
 
-class ScoreGeneration:
-    def __init__(self) -> None:
+    def set_mean(self, mean: int) -> None:
+        pass
+
+    def set_sd(self, sd: int | float) -> None:
+        pass
+
+    def generate_random_distance(self) -> int:
+        pass
+    
+    def calculate_score(self, dist: int) -> int:
+        pass
+
+    def calculate_scores(self, dists: List[int]) -> List[int]:
         pass
