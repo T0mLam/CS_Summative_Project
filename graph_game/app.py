713946f389aae484b674f8f53d53ecce14d33107
lokopopopo <ignatov051@gmail.com>
2024-04-07 21:22:45 +0100

    Fixed the bug with resizing the windows, added back button

--- graph_game/app.py
+++ graph_game/app.py
@@ -1,7 +1,7 @@
-from tkinter import *
 import tkinter as tk
 from tkmacosx import Button
 import pygame
+
 
 class GraphGameGUI(tk.Tk):
     def __init__(self):
@@ -20,14 +20,14 @@ class GraphGameGUI(tk.Tk):
 
         self.soundtrack_state = tk.BooleanVar(value=True)
 
-        # Create frames
-        self.main_menu_frame = MainMenu(self)
-        self.play_frame = Play(self)
-        self.how_to_play_frame = HowToPlay(self)
-        self.leaderboards_frame = Leaderboards(self)
+        # Create a frame dict
+        self.frames = {
+            'menu': MainMenu(self, self.switch_frame, self.soundtrack_state),
+            'play': Play(self, self.switch_frame)
+        }
 
         # Show main menu frame
-        self.main_menu_frame.pack(fill='both', expand=True)
+        self.frames['menu'].pack(fill='both', expand=True)
 
     def switch_soundtrack(self):
         if self.soundtrack_state.get():
@@ -35,133 +35,75 @@ class GraphGameGUI(tk.Tk):
         else:
             pygame.mixer.music.pause()
 
+    def switch_frame(self, current_frame, new_frame):
+        # Hide the current frame
+        self.frames[current_frame].pack_forget()
+        # Minimize the window
+        self.geometry('1x1')
+        # Update the window
+        self.update_idletasks()
+        # Restore the window size
+        self.geometry('800x600')
+        # Show the main menu frame
+        self.frames[new_frame].pack(fill='both', expand=True)
+
 
 class MainMenu(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.master = master
-        self['bg'] = 'white'
+    def __init__(self, parent, switch_frame, soundtrack_state):
+        super().__init__(parent)
+        self.configure(bg="white")
+        self.soundtrack_state = soundtrack_state
+
+        # Play button
+        play_button = Button(self, text="Play", command=lambda: switch_frame('menu', 'play')
+                             , bg='white', fg='black', borderless=1,
+                             font='Helvetica, 40', width=300)
+        play_button.place(relx=0.5, rely=0.3, anchor='center')
 
         # Title
-        title = Label(self, text="Graph Game", font=('Helvetica', 60), bg='white')
+        title = tk.Label(self, text="Graph Game", font=('Helvetica', 60), bg='white')
         title.place(relx=0.5, rely=0.12, anchor='center')
-
-        # Play Button
-        play_button = Button(self, text='Play', bg='white', fg='black', borderless=1, font='Helvetica, 40', width=300,
-                             command=self.show_play_frame)
-        play_button.place(relx=0.5, rely=0.3, anchor='center')
 
         # How To Play Button
         how_to_play_button = Button(self, text='How To Play', bg='white', fg='black', borderless=1,
-                                    font='Helvetica, 40', width=300,
-                                    command=self.show_how_to_play_frame)
+                                    font='Helvetica, 40', width=300)
+        # command=self.show_how_to_play_frame)
         how_to_play_button.place(relx=0.5, rely=0.45, anchor='center')
 
         # Leaderboards Button
         leaderboards_button = Button(self, text='Leaderboards', bg='white', fg='black', borderless=1,
-                                     font='Helvetica, 40', width=300,
-                                     command=self.show_leaderboards_frame)
+                                     font='Helvetica, 40', width=300)
+        # command=self.show_leaderboards_frame)
         leaderboards_button.place(relx=0.5, rely=0.6, anchor='center')
 
         # Quit Button
-        quit_button = Button(self, text='Quit', bg='white', fg='black', borderless=1, font='Helvetica, 40', width=300,
-                             command=self.master.quit)
+        quit_button = Button(self, text='Quit', bg='white', fg='black', borderless=1, font='Helvetica, 40', width=300)
+        # command=self.master.quit)
         quit_button.place(relx=0.5, rely=0.75, anchor='center')
 
         # Soundtrack Switch
-        soundtrack_switch = Checkbutton(self, text='Music', var=self.master.soundtrack_state,
-                                        command=self.master.switch_soundtrack, onvalue=True, offvalue=False,
-                                        bg='white', font='Helvetica, 20')
+        soundtrack_switch = tk.Checkbutton(self, text='Music', var=self.soundtrack_state,
+                                           command=parent.switch_soundtrack,
+                                           onvalue=True, offvalue=False,
+                                           bg='white', font='Helvetica, 20')
         soundtrack_switch.place(relx=0.93, rely=0.95, anchor='center')
-
-    def show_play_frame(self):
-        self.master.play_frame.pack(fill='both', expand=True)
-        self.pack_forget()
-
-    def show_how_to_play_frame(self):
-        self.master.how_to_play_frame.pack(fill='both', expand=True)
-        self.pack_forget()
-
-    def show_leaderboards_frame(self):
-        self.master.leaderboards_frame.pack(fill='both', expand=True)
-        self.pack_forget()
 
 
 class Play(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.master = master
-        self['bg'] = 'white'
+    def __init__(self, parent, switch_frame):
+        super().__init__(parent)
+        self.configure(bg="white")
 
-        # Back Button
-        back_button = Button(self, text='Back to Main Menu', bg='white', fg='black', borderless=1,
-                             font='Helvetica, 20', width=200,
-                             command=self.show_main_menu)
-        back_button.place(relx=0.5, rely=0.9, anchor='center')
+        # Put the back arrow image for the button
+        back_image = tk.PhotoImage(file="./Images/back_arrow.png")
+        # Make the image 10 times smaller
+        resized_back_image = back_image.subsample(10, 10)
 
-    def show_main_menu(self):
-        # Hide the current frame
-        self.pack_forget()
-        # Minimize the window
-        self.master.geometry('1x1')
-        # Update the window
-        self.master.update_idletasks()
-        # Restore the window size
-        self.master.geometry('800x600')
-        # Show the main menu frame
-        self.master.main_menu_frame.pack(fill='both', expand=True)
+        back_button = Button(self, text="Back", command=lambda: switch_frame('play', 'menu'),
+                             image=resized_back_image, background="white", borderless=1, )
+        back_button.pack(side="top", anchor="nw", padx=10, pady=10)
 
 
-class HowToPlay(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.master = master
-        self['bg'] = 'white'
-
-        # Back Button
-        back_button = Button(self, text='Back to Main Menu', bg='white', fg='black', borderless=1,
-                             font='Helvetica, 20', width=200,
-                             command=self.show_main_menu)
-        back_button.place(relx=0.5, rely=0.9, anchor='center')
-
-    def show_main_menu(self):
-        # Hide the current frame
-        self.pack_forget()
-        # Minimize the window
-        self.master.geometry('1x1')
-        # Update the window
-        self.master.update_idletasks()
-        # Restore the window size
-        self.master.geometry('800x600')
-        # Show the main menu frame
-        self.master.main_menu_frame.pack(fill='both', expand=True)
-
-
-class Leaderboards(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.master = master
-        self['bg'] = 'white'
-
-        # Back Button
-        back_button = Button(self, text='Back to Main Menu', bg='white', fg='black', borderless=1,
-                             font='Helvetica, 20', width=200,
-                             command=self.show_main_menu)
-        back_button.place(relx=0.5, rely=0.9, anchor='center')
-
-    def show_main_menu(self):
-        # Hide the current frame
-        self.pack_forget()
-        # Minimize the window
-        self.master.geometry('1x1')
-        # Update the window
-        self.master.update_idletasks()
-        # Restore the window size
-        self.master.geometry('800x600')
-        # Show the main menu frame
-        self.master.main_menu_frame.pack(fill='both', expand=True)
-
-
-if __name__ == '__main__':
-    app = GraphGameGUI()
-    app.mainloop()
+# Create an instance of the GraphGameGUI class and start the application
+gui = GraphGameGUI()
+gui.mainloop()
