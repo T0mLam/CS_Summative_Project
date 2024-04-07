d440af215d73c2a7299cbc91802594440e322bd4
IgnatovArtem <63489540+lokopopopo@users.noreply.github.com>
2024-04-08 00:11:35 +0100

    Finished the play layout

--- graph_game/app.py
+++ graph_game/app.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkmacosx import Button
 import pygame
-
+from tkinter import ttk
 
 class GraphGameGUI(tk.Tk):
     def __init__(self):
@@ -9,7 +9,7 @@ class GraphGameGUI(tk.Tk):
         self.title('Graph Game')
         self.geometry('800x600')
         self['bg'] = 'white'
-        self.resizable(True, True)  # Make the window resizable
+        self.resizable(False, False)  # Make the window resizable
 
         # Initialize pygame mixer for music
         pygame.mixer.init()
@@ -94,14 +94,98 @@ class Play(tk.Frame):
         super().__init__(parent)
         self.configure(bg="white")
 
+        # Balance variable
+        self.balance = int(100)
+        #Balance variable for putting into Label
+        balance_variable = tk.StringVar()
+        balance_variable.set("Balance: " + str(self.balance))
+
+        # Coefficient variable
+        self.coefficient = int(0)
+        # Coefficient variable for putting into Label
+        coefficient_variable = tk.StringVar()
+        coefficient_variable.set("Coefficient: " + str(self.coefficient))
+
+        # Chance of winning variable
+        self.chance_of_winning = int(0)
+        # Chance of winning variable for putting into Label
+        chance_of_winning_variable = tk.StringVar()
+        chance_of_winning_variable.set("Chance of Winning: " + str(self.chance_of_winning) + "%")
+
         # Put the back arrow image for the button
         back_image = tk.PhotoImage(file="./Images/back_arrow.png")
         # Make the image 10 times smaller
         resized_back_image = back_image.subsample(10, 10)
 
-        back_button = Button(self, text="Back", command=lambda: switch_frame('play', 'menu'),
-                             image=resized_back_image, background="white", borderless=1, )
+        back_button = Button(self, command=lambda: switch_frame('play', 'menu'),
+                             image=resized_back_image, background="white", borderless=1)
         back_button.pack(side="top", anchor="nw", padx=10, pady=10)
+
+        # Put the back arrow image for the button
+        buy_image = tk.PhotoImage(file="./Images/shopping_cart.png")
+        # Make the image 10 times smaller
+        resized_buy_image = buy_image.subsample(10, 10)
+
+        # Button for buying credits
+        buy_button = Button(self, command=lambda: switch_frame('play', 'menu'),
+                            image=resized_buy_image, background="white", borderless=1)
+        buy_button.pack(side="top", anchor="ne", padx=10, pady=10)
+
+        #Label with the balance and balance wariable
+        Balance_Label = tk.Label(self, textvariable=balance_variable, bg='white', fg='black', font='Helvetica, 30')
+        Balance_Label.place(relx=0.7, rely=0.05, anchor='center')
+
+        # Label Bid before the scale
+        Bid_Label = tk.Label(self, text = "Bid:", bg='white', fg='black',
+                             font='Helvetica, 30')
+        Bid_Label.place(relx=0.58, rely=0.58, anchor='center')
+
+        # Scale with changing the bid
+        bid_scale = tk.Scale(self, from_=0, to=self.balance, orient=tk.HORIZONTAL,
+                                  length=250,
+                                  bg='white', fg='black',
+                                  font = 'Helvetica, 30',
+                                  )
+        bid_scale.place(relx=0.8, rely=0.55, anchor='center')
+
+        # Starting node label
+        Starting_node = ttk.Combobox(self, width=5)
+        Starting_node['values'] = ('1', '2', '3', '4','5', '6', '7','8', '9', '10', '11', '12', '13','14','15')
+        Starting_node.place(relx=0.84, rely=0.36, anchor='center')
+
+        # Starting node combobox
+        Starting_node_Label = tk.Label(self, text = "Starting node: ", bg='white', fg='black',
+                                       font='Helvetica, 30')
+        Starting_node_Label.place(relx=0.67, rely=0.35, anchor='center')
+
+        # Ending node label
+        Ending_node = ttk.Combobox(self, width=5)
+        Ending_node['values'] = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15')
+        Ending_node.place(relx=0.84, rely=0.46, anchor='center')
+
+        # Starting node combobox
+        Ending_node = tk.Label(self, text="Ending node: ", bg='white', fg='black',
+                                       font='Helvetica, 30')
+        Ending_node.place(relx=0.66, rely=0.45, anchor='center')
+
+        # Coefficient label with coefficient variable
+        Coefficient_label = tk.Label(self, textvariable=coefficient_variable, bg='white', font='Helvetica, 30')
+        Coefficient_label.place(relx=0.66, rely=0.68, anchor='center')
+
+        # Chance of winning label with chance of winning variable
+        Chance_Of_Winning_label = tk.Label(self, textvariable=chance_of_winning_variable, bg='white', font='Helvetica, 30')
+        Chance_Of_Winning_label.place(relx=0.74, rely=0.78, anchor='center')
+
+        Bet_Button = Button(self, text = "BET", bg = 'white', fg = 'black',
+                               font='Helvetica, 30',
+                               borderless=1,
+                               width=305)
+        Bet_Button.place(relx=0.74, rely=0.9, anchor='center')
+
+        canvas = tk.Canvas(self, width=400, height=400, bg="white")
+        canvas.place(relx=0.27, rely=0.64, anchor='center')
+
+
 
 
 # Create an instance of the GraphGameGUI class and start the application
