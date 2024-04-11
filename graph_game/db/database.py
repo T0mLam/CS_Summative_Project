647fe32d4c17041cd373319fa0db37ec6d4a2219
Oluwafemi okono <148986193+fokono18@users.noreply.github.com>
2024-04-11 16:10:09 +0000

    database.py framework

---
+++ graph_game/db/database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+class DatabaseConnection:
+
+    """A context manager class for handling database connections."""
+
+    def __init__(self, db_name):
+
+    def __enter__(self):
+       
+    def __exit__(self, exc_type, exc_val, exc_tb):
+
+def initialize_database(db_name):
+    
+def register_player(name, initial_balance, db_name):
+    
+def log_game(player_id, nodes, result, db_name):
+    
