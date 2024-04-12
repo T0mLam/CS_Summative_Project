3fa611b11a6e4d8881c06a5dc7f38c4c0706b444
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-04-12 22:47:39 +0100

    add a template for search engine

--- graph_game/search_engine/search_engine.py
+++ graph_game/search_engine/search_engine.py
@@ -1,7 +1,21 @@
+from typing import List
 from db.database import DatabaseConnection
 from utils.trie import Trie
 
 
 class SearchEngine:
     def __init__(self) -> None:
+        self.trie = Trie()
+        self.fetch_all_users()
+        
+    def complete_search(self, name: str) -> List[str]:
+        return self.trie.complete(name)
+    
+    def search_result(self, name: str) -> List[str]:
+        players = self.trie.fizzy_search(name, threshold=3, num_return=10)
+
+    def get_leaders(self, n: int) -> List[str]:
+        pass
+
+    def fetch_all_users(self) -> None:  
         pass
