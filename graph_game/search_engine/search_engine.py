ca80d2773812181eca630c2f901e29281b03b463
TomLam <136619817+T0mLam@users.noreply.github.com>
2024-04-13 22:49:32 +0100

    finish search_result, get_leaders and fetch_users

--- graph_game/search_engine/search_engine.py
+++ graph_game/search_engine/search_engine.py
@@ -1,28 +1,52 @@
-from typing import List
+from typing import List, Tuple
 from ..db.database import DatabaseConnection
 from ..utils.trie import Trie
 
 
 class SearchEngine:
     def __init__(self) -> None:
-        self.trie = Trie()
-        self.fetch_all_users()
+        self.trie = None
+        self.fetch_all_users_to_trie()
         
     def complete_search(self, name: str) -> List[str]:
         return self.trie.complete(name)
     
     def search_result(self, name: str) -> List[str]:
-        players = self.trie.fizzy_search(name, threshold=3, num_return=10)
+        players = self.trie.fizzy_search(name, threshold=2, num_return=10)
 
-    def get_leaders(self, n: int) -> List[str]:
+        res = []
+
         with DatabaseConnection('db') as conn:
-            leaders = conn.get_cursor().execute(
-                # add your code here
-            )
+            for player in players:
+                player_name_score = conn.get_cursor().execute(
+                    'SELECT name, balance FROM players WHERE name = ?', (player,)
+                ).fetchone()
 
-    def fetch_all_users(self) -> None:  
-        pass
+                res.append(player_name_score)
+
+        return res
+
+    def get_leaders(self, n: int) -> List[Tuple[str]]:
+        with DatabaseConnection('db') as conn:
+            leaders_name_score = conn.get_cursor().execute(
+                'SELECT name, balance FROM players ORDER BY balance DESC'
+            ).fetchall()
+            
+        return leaders_name_score
+
+    def fetch_all_users_to_trie(self) -> None:  
+        with DatabaseConnection('db') as conn:
+            players = conn.get_cursor().execute(
+                'SELECT name FROM players'
+            ).fetchall()
+        
+        self.trie = Trie()
+
+        for player in players:
+            self.trie.insert(*player)
 
 
 if __name__ == '__main__':
+    se = SearchEngine()
+    print(se.complete_search('Fem'))
     print('success')
