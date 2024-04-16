d99586c8faeb1d7d976b4e0f446eb03c8300af3f
Zifan05 <3171149409@qq.com>
2024-04-16 10:11:54 +0800

    heap & randomised_set test

---
+++ tests/test_heap.py
@@ -0,0 +1,74 @@
+import unittest
+from graph_game.utils.node import Node
+from graph_game.utils.heap import MinHeap
+
+
+class TestHeap(unittest.TestCase):
+    def setUp(self):
+        """Set up test fixtures"""
+        self.nodes = [Node(i) for i in range(10)]
+
+    def test_init_heap(self):
+        """Test initializing heap"""
+        heap = MinHeap([(3, self.nodes[3]), (1, self.nodes[1]), (2, self.nodes[2])])
+        self.assertEqual(heap.pop()[0],1)
+        self.assertEqual(heap.pop()[0],2)
+        self.assertEqual(heap.pop()[0],3)
+
+    def test_push_pop(self):
+        """Test push and pop to the heap"""
+        heap = MinHeap()
+        heap.push((2, self.nodes[2]))
+        heap.push((1, self.nodes[1]))
+        heap.push((3, self.nodes[3]))
+        self.assertEqual(heap.pop()[0], 1)
+        self.assertEqual(heap.pop()[0], 2)
+        self.assertEqual(heap.pop()[0], 3)
+
+    def test_heap_property(self):
+        """Test heap property"""
+        heap = MinHeap()
+        values = [1,2,3,4,5,6,7,8,9]
+        for value in values:
+            heap.push((value, Node(value)))
+        result = []
+        while len(heap) > 0:
+            result.append(heap.pop()[0])
+        self.assertEqual(result, sorted(values))
+
+    def test_top(self):
+        """Test the top element of the heap"""
+        heap = MinHeap([(2, self.nodes[2]), (1, self.nodes[1]), (3, self.nodes[3])])
+        self.assertEqual(heap.top()[0], 1)
+
+    def test_empty_heap(self):
+        """Test empty heap"""
+        heap = MinHeap()
+        self.assertIsNone(heap.top())
+        self.assertIsNone(heap.pop())
+
+    def test_duplicate(self):
+        """Test duplicates into heap"""
+        heap = MinHeap([(2, self.nodes[0]),(2, self.nodes[1]), (2, self.nodes[2])])
+        heap.push((2, self.nodes[4]))
+        heap.push((1, self.nodes[3]))
+        self.assertEqual(heap.top()[0],1)
+        heap.pop()
+        for _ in range(4):
+            self.assertEqual(heap.top()[0],2)
+            heap.pop()
+
+    def test_mixed_heap(self):
+        """Test mixed situation"""
+        heap = MinHeap()
+        values = [(2, self.nodes[2]), (1, self.nodes[1]), (3, self.nodes[3])]
+        for val in values:
+            heap.push(val)
+        self.assertEqual(heap.pop()[0],1)
+        heap.push((0, self.nodes[0]))
+        self.assertEqual(heap.pop()[0],0)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
---
+++ tests/test_randomised_set.py
@@ -0,0 +1,55 @@
+import random
+import unittest
+from graph_game.utils.randomised_set import RandomisedSet
+
+class TestRandomisedSet(unittest.TestCase):
+    def setUp(self):
+        """INITIALIZE a RandomisedSet object"""
+        self.randomised_set = RandomisedSet()
+        random.seed(0)
+
+    def test_initialization(self):
+        """The edges and edge_to_idx of the RandomisedSet should be empty"""
+        self.assertFalse(self.randomised_set.edges)
+        self.assertFalse(self.randomised_set.edge_to_idx)
+
+    def test_add_edge(self):
+        """Add an edge to the RandomisedSet"""
+        self.randomised_set.add_edge_to_set(1, 2)
+        self.assertEqual(self.randomised_set.edges, [(1, 2)])
+        self.assertEqual(self.randomised_set.edge_to_idx, {(1, 2): 0})
+
+
+    def test_remove_edge(self):
+        """Remove an edge from the RandomisedSet"""
+        self.randomised_set.add_edge_to_set(1, 2)
+        self.randomised_set.remove_edge_from_set(1, 2)
+        self.assertNotIn((1, 2),self.randomised_set.edge_to_idx)
+        self.assertFalse(self.randomised_set.edges)
+
+    def test_add_duplicate_edges(self):
+        """Can not add duplicate edges to the RandomisedSet"""
+        self.randomised_set.add_edge_to_set(1, 2)
+        self.randomised_set.add_edge_to_set(2, 1)
+        self.assertEqual(len(self.randomised_set.edges), 1)
+
+    def test_random_edge(self):
+        """Random edge extraction should correctly remove the edge from the set."""
+        edges_to_add = [(1, 2), (3, 4)]
+        for edge in edges_to_add:
+            self.randomised_set.add_edge_to_set(*edge)
+        extracted_edges = self.randomised_set.get_random_edge()
+        self.assertIn(extracted_edges, edges_to_add)
+        self.assertEqual(len(self.randomised_set.edges), 1)
+        self.assertNotIn(extracted_edges, self.randomised_set.edge_to_idx)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
